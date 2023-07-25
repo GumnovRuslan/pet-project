724c969b0d09f65615c362811dddd8ea29568ff7
Gumnov Ruslan <ruslang.crocode@gmail.com>
2023-07-25 18:09:55 +0300

    add in setting svg and slider

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const logIn = document.querySelector('.profile__log')
 const profilePhoto = document.querySelector('.profile__photo')
 const settingMenu = document.querySelector('.profile__setting')
 const links = document.querySelector('.profile__links')
+const menu = document.querySelector('.setting__menu')
+const exit = menu.querySelectorAll('.setting__link')[3]
+console.log(exit)
 
 profilePhoto.style.display = 'none'
 settingMenu.style.display = 'none'
@@ -9,6 +12,7 @@ links.style.display = 'none'
 
 logIn.addEventListener('click', comeIn)
 profilePhoto.addEventListener('click', openSetting)
+exit.addEventListener('click', leave)
 
 function comeIn() {
 	profilePhoto.style.display = 'block'
@@ -18,4 +22,11 @@ function comeIn() {
 function openSetting() {
 	if (settingMenu.style.display == 'none') settingMenu.style.display = 'block'
 	else settingMenu.style.display = 'none'
+}
+
+function leave() {
+	settingMenu.style.display = 'none'
+	profilePhoto.style.display = 'none'
+	links.style.display = 'none'
+	logIn.style.display = 'block'
 }
