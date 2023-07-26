fe4d65766e6778ceaa3e648862470be60f0609b4
Gumnov Ruslan <ruslang.crocode@gmail.com>
2023-07-26 10:24:03 +0300

    add icon

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const settingMenu = document.querySelector('.profile__setting')
 const links = document.querySelector('.profile__links')
 const menu = document.querySelector('.setting__menu')
 const exit = menu.querySelectorAll('.setting__link')[3]
-console.log(exit)
+const theme = menu.querySelectorAll('.setting__link')[2]
+const checkbox = document.querySelector('.checkbox')
+// console.log(checkbox)
 
 profilePhoto.style.display = 'none'
 settingMenu.style.display = 'none'
@@ -13,12 +15,14 @@ links.style.display = 'none'
 logIn.addEventListener('click', comeIn)
 profilePhoto.addEventListener('click', openSetting)
 exit.addEventListener('click', leave)
+theme.addEventListener('click', changeTheme)
 
 function comeIn() {
 	profilePhoto.style.display = 'block'
 	logIn.style.display = 'none'
 	links.style.display = 'flex'
 }
+
 function openSetting() {
 	if (settingMenu.style.display == 'none') settingMenu.style.display = 'block'
 	else settingMenu.style.display = 'none'
@@ -29,4 +33,9 @@ function leave() {
 	profilePhoto.style.display = 'none'
 	links.style.display = 'none'
 	logIn.style.display = 'block'
+}
+
+function changeTheme() {
+	if (checkbox.checked) checkbox.checked = false
+	else checkbox.checked = true
 }
