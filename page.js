7a55625d2a548b774c98c6f1dd5cfdfac001dae1
Gumnov Ruslan <ruslang.crocode@gmail.com>
2023-08-07 15:09:30 +0300

    add comments DB

--- page.js
+++ page.js
@@ -27,3 +27,50 @@ reveal.addEventListener('click', (e) => {
 })
 
 // const textArea = document.querySelector('.comment__input')
+
+// COMMENTS
+const commentsUrl = 'https://jsonplaceholder.typicode.com/comments'
+// let commentsArr = []
+
+const comments = document.querySelector('.comments__inner')
+
+function draw(user, comment, mail) {
+	let comItem = document.createElement('div')
+	comItem.classList.add('comment__item')
+
+	let comUser = document.createElement('p')
+	comUser.classList.add('comment__user')
+	comUser.textContent = user
+
+	let email = document.createElement('p')
+	email.classList.add('comment__email')
+	email.textContent = mail
+
+	let comBody = document.createElement('p')
+	comBody.classList.add('comment__body')
+	comBody.textContent = comment
+
+	comments.append(comItem)
+	comItem.append(comUser)
+	comItem.append(email)
+	comItem.append(comBody)
+}
+
+function com(e) {
+	if (comments.querySelectorAll('.comment__item').length < 10) {
+		fetch(commentsUrl)
+			.then((response) => response.json())
+			.then((commits) => (commentsArr = commits.slice(0)))
+			.then((res) => {
+				for (let i = 0; i < 10; i++) {
+					draw(res[i].name, res[i].body, res[i].email)
+				}
+			})
+	}
+
+	if ((comments.style.display = 'none')) comments.style.display = 'flex'
+	this.style.display = 'none'
+}
+
+const btn = document.querySelector('.comments__show')
+btn.addEventListener('click', com)
