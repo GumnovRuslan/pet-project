3fe8ee1540bef929a57c17c2bd0925df339baf32
Gumnov Ruslan <ruslang.crocode@gmail.com>
2023-08-08 12:03:11 +0300

    add gulp 2

--- footer.js
+++ footer.js
@@ -2,13 +2,24 @@ let fInner = document.querySelector('.footer__inner')
 let block = fInner.querySelectorAll('.footer__list')
 
 for (let i = 0; i < block.length; i++) {
-	block[i].addEventListener('click', openList)
+	block[i]
+		.querySelector('.footer__block-header')
+		.addEventListener('click', openList)
 }
 
 function openList() {
-	const listBody = this.querySelector('.footer__block-body')
-	const listImg = this.querySelector('.footer__title-img')
+	const block = this.closest('.footer__list')
+	const header = this
+	const listBody = block.querySelector('.footer__block-body')
+	const listImg = block.querySelector('.footer__title-img')
 
-	listBody.style.cssText = `display: flex;`
-	listImg.style.transform = 'rotate(180deg)'
+	if (!listBody.style.display) {
+		listBody.style.cssText = `display: flex;`
+		listImg.style.transform = 'rotate(180deg)'
+		header.style.cssText = `background: #000;`
+	} else {
+		listBody.style.display = ''
+		listImg.style.transform = 'rotate(0deg)'
+		header.style.cssText = `background: transparent;`
+	}
 }
